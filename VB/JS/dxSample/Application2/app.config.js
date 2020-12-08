fc384cf5e108f4340798fe7c5cbd721080208521
DevExpressExampleBot <fakeEmail>
2020-12-08 19:41:00 +0300

    Source auto update [skip ci]

---
+++ VB/JS/dxSample/Application2/app.config.js
@@ -0,0 +1,12 @@
+﻿window.Application2 = $.extend(true, window.Application2, {
+  "config": {
+    "defaultLayout": "navbar",
+    "navigation": [
+      {
+        "title": "Index",
+        "action": "#Index",
+        "icon": "home"
+      }
+    ]
+  }
+});
---
+++ VB/JS/dxSample/Application2/app.js
@@ -0,0 +1,12 @@
+﻿window.Application2 = window.Application2 || {};
+
+$(function() {
+    app = new DevExpress.framework.html.HtmlApplication({
+        ns: Application2,
+        viewPortNode: document.getElementById("viewPort"),
+        defaultLayout: Application2.config.defaultLayout,
+        navigation: Application2.config.navigation,
+    });
+    app.router.register(":view/:id", { view: "Index", id: undefined });
+    
+});
